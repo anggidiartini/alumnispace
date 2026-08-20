fdc9f5ca666643fe528a5c6081f03254d3e8065d
anggidiartini-jpg <anggi51805@gmail.com>
2026-08-20 14:43:53 +0800

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('landing.index');
+});
+
+Route::get('/login', function () {
+    return view('login.index');
+});
+
+Route::get('/home', function () {
+    return view('home.index');
+});
+
+Route::get('/alumni', function () {
+    return view('alumni.index');
+});
+
+Route::get('/alumni/detail', function () {
+    return view('alumni.detail');
+});
+
+Route::get('/lowongan', function () {
+    return view('lowongan.index');
+});
+
+Route::get('/event', function () {
+    return view('event.index');
+});
+
+Route::get('/album', function () {
+    return view('album.index');
+});
+
+Route::get('/admin', function () {
+    return view('admin.dashboard.index');
+});
