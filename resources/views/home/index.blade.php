9f5ae3138f1046e1351680c975350008ea1629a3
Dinda <dindazahra053@gmail.com>
2026-08-31 15:05:28 +0800

    terbaru

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,28 +9,7 @@
 </head>
 <body>
     <x-navbar />
-    <!-- NAVBAR TRANSPARAN -->
-<header class="navbar-container">
-    <div class="navbar-logo">
-        ALUMNI HUB
-    </div>
 
-    <!-- Tombol Garis Tiga (Hamburger) untuk HP -->
-    <div class="menu-toggle" id="menuToggle">
-        <span></span>
-        <span></span>
-        <span></span>
-    </div>
-
-    <!-- Menu Navbar (Href dikosongkan/diatur ke # agar tidak pindah section) -->
-    <nav class="navbar-menu" id="navbarMenu">
-        <a href="#" class="active">Beranda</a>
-        <a href="#">Lowongan</a>
-        <a href="#">Alumni</a>
-        <a href="#">Event</a>
-        <a href="#">Album</a>
-    </nav>
-</header>
 
     <!-- KONTEN UTAMA: 5 SECTION -->
     <main>
