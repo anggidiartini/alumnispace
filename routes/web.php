ddc97fb2578ca854a3759c7cf870d4776caa5da3
khanyaparastya-cmd <khanyaparastya@gmail.com>
2026-09-02 10:25:55 +0800

    artikel folder

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/landing', function () {
 });
 
 Route::get('/login', function () {
-    return view('login.index');
+    return view('auth.login');
 })->name('login');
 
 Route::get('/home', function () {
