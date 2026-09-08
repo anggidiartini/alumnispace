1f742320158d6d71bfc11075a707166a63dd1700
MunOpensss <yuaprilsc@gmail.com>
2026-09-08 11:47:39 +0800

    add socials

--- database/migrations/2026_09_08_034010_create_companies_table_and_add_foreign_to_job_vacancies.php
+++ database/migrations/2026_09_08_034010_create_companies_table_and_add_foreign_to_job_vacancies.php
@@ -4,16 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-// Class dummy kosong agar Laravel tidak error saat membaca riwayat commit Git
 return new class extends Migration
 {
     public function up(): void
     {
-        //
+        // Sengaja dikosongkan karena tabel 'companies' dan kolom 'company_id' 
+        // sudah Anda masukkan secara manual langsung di dalam MySQL.
+        // File ini hanya berfungsi sebagai penanda 'DONE' bagi Laravel.
     }
 
     public function down(): void
     {
-        //
+        // Kosongkan juga
     }
 };
