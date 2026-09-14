af728f9037e078f39d326f5b7e75b884c7cad591
ayucitra2913-cmd <ayucitra2913@gmail.com>
2026-09-14 13:34:23 +0800

    Move JobVacancyController to User namespace

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\JobVacancyController;
+use App\Http\Controllers\User\JobVacancyController;
 use App\Http\Controllers\EventController;
 use App\Http\Controllers\AlbumController;
 use App\Http\Controllers\AlumniDirectoryController;
