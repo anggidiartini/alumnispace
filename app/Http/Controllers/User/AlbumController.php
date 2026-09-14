5981ba107b6b22e05f57897780a362e681996fa3
ayucitra2913-cmd <ayucitra2913@gmail.com>
2026-09-14 14:03:10 +0800

    Move AlbumController to User namespace

---
+++ app/Http/Controllers/User/AlbumController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Album;
+
+class AlbumController extends Controller
+{
+    public function index(Request $request)
+    {
+        $query = Album::with('photos');
+
+        if ($request->filled('category') && $request->category !== 'all') {
+            $query->where('category', $request->category);
+        }
+
+        $albums = $query->orderBy('event_date', 'desc')->get();
+        $totalAlbums = Album::count();
+
+        return view('album.index', compact('albums', 'totalAlbums'));
+    }
+
+    public function show($slug)
+    {
+        $album = Album::with('photos')->where('slug', $slug)->firstOrFail();
+        return view('album.detail', compact('album'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\LandingController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\User\JobVacancyController;
 use App\Http\Controllers\User\EventController;
-use App\Http\Controllers\AlbumController;
+use App\Http\Controllers\User\AlbumController;
 use App\Http\Controllers\AlumniDirectoryController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ArticleController;
